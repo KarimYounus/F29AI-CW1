a5c2e8749f4e09f21ccbf6050530f3ab04323c6f
kfy2000 <kfy2000@hw.ac.uk>
2022-10-16 14:50:18 +0100

    Adding Manhatten distance

--- JavaSearch-2022/src/uk/ac/hw/macs/search/GridState.java
+++ JavaSearch-2022/src/uk/ac/hw/macs/search/GridState.java
@@ -1,43 +1,46 @@
 package uk.ac.hw.macs.search;
 
+import uk.ac.hw.macs.search.State;
+
+
 public class GridState implements State{
 
 
     private int x, y;
     private int squareCost;
-    private int heuristic;
     private boolean goal;
+    private int heuristic;
 
     public GridState(int x, int y) {
         this.x = x;
         this.y = y;
         this.squareCost = 1;
-        this.heuristic = 0;
         this.goal = false;
+        this.heuristic = 0;
     }
 
     public GridState(int x, int y, int squareCost) {
         this.x = x;
         this.y = y;
         this.squareCost = squareCost;
+        this.goal = false;
         this.heuristic = 0;
-        this.goal = false;
     }
 
-    public GridState(int x, int y, int squareCost, int heuristic) {
+    public GridState(int x, int y, int squareCost, boolean goal) {
         this.x = x;
         this.y = y;
         this.squareCost = squareCost;
-        this.heuristic = heuristic;
-        this.goal = false;
-    }
-
-    public GridState(int x, int y, int squareCost, int heuristic, boolean goal) {
-        this.x = x;
-        this.y = y;
-        this.squareCost = squareCost;
-        this.heuristic = heuristic;
+        this.heuristic = 0;
         this.goal = goal;
+    }
+
+    public GridState(int x, int y, int squareCost, boolean goal, Node target) throws Exception {
+        this.x = x;
+        this.y = y;
+        this.squareCost = squareCost;
+        this.goal = goal;
+        this.heuristic = calculateHeuristic(target);
     }
 
     //Returns coordinates in int array
@@ -60,7 +63,14 @@ public class GridState implements State{
     }
 
     //Calculated using Manhattan distance
-    private int calculateHeuristic() {
+    private int calculateHeuristic(Node target) throws Exception {
+
+        //Check that target node is indeed the goal node, if not then throw exception
+        if (!target.isGoal()) {
+            throw new Exception("Node is not a goal node, can't calculate heuristic");
+        }
+
+//        int manhattenDistance = Math.abs(target.getValue().
 
         return 0;
     }
